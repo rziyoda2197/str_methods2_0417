eb0ea9746f88d06e3c8149c78613857a5a115e6b
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-17 16:56:32 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#1-misol
+matn = "Python"
+print(matn.rjust(25))
+# Natija:                    Python
+
+#2-misol
+matn = "42"
+print(matn.zfill(6))    # Natija: 000042
+
+#3-misol
+matn = "banana"
+print(matn.replace("a", "@"))   # Natija: b@n@n@
+
+#4-misol
+matn = "Men Python va Django o'rganaman"
+print(matn.find("Django"))      # Natija: 14
+
